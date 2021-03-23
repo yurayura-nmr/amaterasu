6b35477cb4d70fd3ecaac1466fa725749905596b
yurayura-nmr <81198401+yurayura-nmr@users.noreply.github.com>
2021-03-23 21:24:44 +0900

    Update a_cout.py

--- amaterasux/a_cout.py
+++ amaterasux/a_cout.py
@@ -1,32 +1,25 @@
 import sys
 
-
 def err(message):
     """
     Prints error message and terminates the program.
     """
-
     nprint('ERROR', message)
     sys.exit()
-
 
 def nprint(a, b):
     """
     Left-aligned command line output.
     """
-
     print "" + str(a).ljust(40) + str(b).ljust(60)
-
 
 def printf(format, *args):
     sys.stdout.write(format % args)
-
 
 def printParameters(self):
     """
     Show the parameters for the Amaterasu run.
     """
-
     nprint("\nAcquisition Parameters \n", "")
     nprint("Data directory", self.dataDir)
 
