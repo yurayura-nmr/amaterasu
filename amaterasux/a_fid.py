944fefaad783b0aad7b19a80b4d44e4f3a684c15
yurayura-nmr <81198401+yurayura-nmr@users.noreply.github.com>
2021-03-23 22:02:34 +0900

    Update a_fid.py
    Started to add option for off-resonance R1rho.

--- amaterasux/a_fid.py
+++ amaterasux/a_fid.py
@@ -2,21 +2,21 @@ class fid:
 
     def __init__(self):
         """
-        Parameters describing a given FID/spectrum belonging to
-        a certain resonance at a certain spin-lock power
+        Parameters describing a given fid or equivalently, the resulting spectrum belonging to
+        a certain resonance at a certain spin-lock power or 
+                            at a certain spin-lock offset
 
         Index: e.g. 0 to 24 for 1.fid to 25.fid
         """
-
         self.reference = False
         self.filename = ''
         self.index = 0
         self.spinlockHz = 0
+        self.spinlockOffset = 0 # testing
 
         """
         Parameters needed to FT the current FID
         """
-
         self.apodizationOffset = 0.4
         self.apodizationEnd = 0.95
         self.apodizationPower = 1
