00b128367402148a1ce507c506e79f6cacc90040
yurayura-nmr <81198401+yurayura-nmr@users.noreply.github.com>
2021-03-23 21:35:11 +0900

    Update a_dependency.py

--- amaterasux/a_dependency.py
+++ amaterasux/a_dependency.py
@@ -4,10 +4,8 @@ import sys
 
 from a_cout import *
 
-
 def cmd_exists(cmd):
     return subprocess.call("type " + cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
-
 
 def checkModule(module):
     try:
@@ -17,14 +15,12 @@ def checkModule(module):
         nprint("Checking for python module:" + module, "... MISSING.")
         sys.exit()
 
-
 def checkCommand(cmd):
-    if cmd_exists(cmd) == True:
+    if cmd_exists(cmd) is True:
         nprint("Checking for executable:" + cmd, "... found.")
-    elif cmd_exists(cmd) == False:
+    elif cmd_exists(cmd) is False:
         nprint("Checking for executable:" + cmd, "... MISSING.")
         sys.exit()
-
 
 def checkModules():
     nprint("Checking if required modules are installed", "")
@@ -33,10 +29,8 @@ def checkModules():
     checkModule('nmrglue')
 
     import nmrglue
-    if "0.4" in nmrglue.__version__:
-        err("nmrglue version found is too low. Version 0.6 or higher is required. Please see INSTALL.txt for instructions how to install nmrglue.")
-    elif "0.5" in nmrglue.__version__:
-        err("nmrglue version found is too low. Version 0.6 or higher is required. Please see INSTALL.txt for instructions how to install nmrglue")
+    if nmrglue.__version__ < 0.6:
+        err("nmrglue version found is too low. Version 0.6 or higher is required. Please see INSTALL.txt for instructions how to install nmrglue.")       
     else:
         pass
 
