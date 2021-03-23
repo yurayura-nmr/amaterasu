ec89a92dd4c5342724476c6469ea82ad0f65c5d8
yurayura-nmr <81198401+yurayura-nmr@users.noreply.github.com>
2021-03-23 21:21:25 +0900

    Update a_residue.py

--- amaterasux/a_residue.py
+++ amaterasux/a_residue.py
@@ -1,8 +1,7 @@
 class residue:
-
     def __init__(self):
-        self.wH = 0.00             # CNST1
-        self.wN = 0.00             # O3P
+        self.wH = 0.00             # CNST1; peak expected from spin-lock selection
+        self.wN = 0.00             # O3P;   peak expected from spin-lock selection
 
-        self.wH_observed = 0.00    # peak observed (ppm)
+        self.wH_observed = 0.00    # peak observed in the experiment (ppm)
         self.index = 0             # position in fq1list
